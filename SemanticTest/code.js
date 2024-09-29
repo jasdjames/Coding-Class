475334cfa3fe6ccd08d803c5d6ed0e5fef4abcc3
Jas James <jasdjames@gmail.com>
2024-09-29 10:57:37 -0500

    First code push

---
+++ SemanticTest/code.js
@@ -0,0 +1,25 @@
+
+
+let Jas = 'Jas James'
+console.log(Jas);
+const x = 7 * 8
+const animals = ['lion', 'tiger', 'bear', 'zebra']
+
+Jas = "J. James"
+
+console.log(Jas);
+
+console.log(x, 'THIS IS X');
+
+console.log(animals[0], 'not Zerba here');
+
+
+if (animals[0] === 'lion') { console.log('Hello');
+
+    
+} else {
+    console.log('Goodbye');
+    
+}
+
+
---
+++ SemanticTest/readme.md
@@ -0,0 +1,8 @@
+# This is my Readme
+## It will not be semantic
+
+-- But doesn't it look good 
+
+- [x] Write the press release
+- [ ] **Update the website**
+- [ ] *Contact the media*
