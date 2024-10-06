ed44fe549f4bac9e039073f07efb6df259014fe9
Jas James <jasdjames@gmail.com>
2024-10-06 18:10:00 -0500

    Adding code for Funtions

--- SemanticTest/code.js
+++ SemanticTest/code.js
@@ -14,12 +14,41 @@ console.log(x, 'THIS IS X');
 console.log(animals[0], 'not Zerba here');
 
 
-if (animals[0] === 'lion') { console.log('Hello');
+// if (animals[0] === 'lion') { console.log('Hello');
 
     
-} else {
+// } else {
+//     console.log('Goodbye');
+    
+// }
+
+//FUNCTION -- Log Hello / Goodbye -- Call Funtion / return 
+
+function helloClass(){
+    console.log('Hello');
     console.log('Goodbye');
     
 }
+
+helloClass();
+
+function adding(a, b){
+
+    return a+b 
+}
+
+const value = adding(3,10)
+
+console.log('The value is' , value);
+
+
+const evenArray =  [2, 4, 6, 8, 10]
+const oddArray = [1,3,5,7,9]
+
+for (var i = 0; i< evenArray.length; i++){
+    console.log(evenArray[i]);
+    
+}
+
 
 
